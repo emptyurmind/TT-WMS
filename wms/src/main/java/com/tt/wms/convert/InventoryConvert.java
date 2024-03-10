98bee057f38d0b20da8f80ad0503ff4488a88d55
emptyurmind <2390757768@qq.com>
2024-03-10 21:20:53 +0800

    add inventory service

---
+++ wms/src/main/java/com/tt/wms/convert/InventoryConvert.java
@@ -0,0 +1,18 @@
+package com.tt.wms.convert;
+
+import com.tt.wms.domain.entity.InventoryHistory;
+import org.mapstruct.Mapper;
+import com.tt.wms.domain.entity.Inventory;
+import com.tt.wms.domain.vo.InventoryVO;
+import java.util.List;
+/**
+ * 库存  ENTITY <=> VO / Form / Query
+ *
+ * @auhtor wangkun
+ */
+@Mapper(componentModel = "spring")
+public interface InventoryConvert  {
+    List<InventoryVO> dos2vos(List<Inventory> list);
+
+    Inventory inventoryHistory2invertory(InventoryHistory it);
+}
