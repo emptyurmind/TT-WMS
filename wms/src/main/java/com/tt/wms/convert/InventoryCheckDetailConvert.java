15c605b99cf2fbec3b04a35c5e7e130794a4bcbf
emptyurmind <2390757768@qq.com>
2024-03-11 11:04:32 +0800

    add inventory check service

---
+++ wms/src/main/java/com/tt/wms/convert/InventoryCheckDetailConvert.java
@@ -0,0 +1,20 @@
+package com.tt.wms.convert;
+
+import com.tt.wms.domain.entity.InventoryHistory;
+import org.mapstruct.Mapper;
+import com.tt.wms.domain.entity.InventoryCheckDetail;
+import com.tt.wms.domain.vo.InventoryCheckDetailVO;
+import java.util.List;
+/**
+ * 库存盘点单据详情  ENTITY <=> VO / Form / Query
+ *
+ * @auhtor wangkun
+ */
+@Mapper(componentModel = "spring")
+public interface InventoryCheckDetailConvert  {
+    List<InventoryCheckDetailVO> dos2vos(List<InventoryCheckDetail> list);
+
+    List<InventoryCheckDetail> vos2dos(List<InventoryCheckDetailVO> details);
+
+    InventoryHistory vo2InventoryHistory(InventoryCheckDetailVO it);
+}
---
+++ wms/src/main/java/com/tt/wms/domain/form/InventoryCheckFrom.java
@@ -0,0 +1,21 @@
+package com.tt.wms.domain.form;
+
+import com.tt.wms.domain.entity.InventoryCheck;
+import com.tt.wms.domain.vo.InventoryCheckDetailVO;
+import com.tt.wms.domain.vo.ItemVO;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 盘库单据 数据视图对象
+ *
+ * @author wangkun
+ */
+@Data
+public class InventoryCheckFrom extends InventoryCheck {
+    // 盘库单据详情
+    private List<InventoryCheckDetailVO> details;
+    // 所有商品
+    private List<ItemVO> items;
+}
