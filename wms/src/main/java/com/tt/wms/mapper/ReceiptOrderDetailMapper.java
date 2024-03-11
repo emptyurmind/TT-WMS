fe7c11806ea2d0e8d0519f7a071cea8ac996fbd4
emptyurmind <2390757768@qq.com>
2024-03-11 12:36:13 +0800

    add receipt order service

---
+++ wms/src/main/java/com/tt/wms/mapper/ReceiptOrderDetailMapper.java
@@ -0,0 +1,36 @@
+package com.tt.wms.mapper;
+
+import java.util.Collection;
+import java.util.List;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tt.wms.domain.vo.ReceiptOrderVO;
+import org.apache.ibatis.annotations.Param;
+import com.tt.wms.domain.entity.ReceiptOrderDetail;
+
+/**
+ * 入库单详情Mapper接口
+ * 
+ * @auhtor wangkun
+ */
+public interface ReceiptOrderDetailMapper extends BaseMapper<ReceiptOrderDetail> {
+    /**
+     * 查询入库单详情列表
+     *
+     * @param receiptOrderDetail 入库单详情
+     * @return 入库单详情集合
+     */
+    List<ReceiptOrderDetail> selectByEntity(ReceiptOrderDetail receiptOrderDetail);
+
+    /**
+     * 批量软删除
+     * @param ids
+     * @return
+    */
+    int updateDelFlagByIds(@Param("ids") Long[] ids);
+
+    int batchInsert(List<ReceiptOrderDetail> details);
+
+    List<ReceiptOrderVO> countByOrderId(@Param("ids") Collection<Long> ids);
+
+    List<ReceiptOrderDetail> selectDetailByWaveNo(String waveNo);
+}
